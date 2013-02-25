fed9f45ea3c265c9cc3f9658f6cebe3b3b09e202
Ramón Roche <mrpollo@gmail.com>
2013-02-25 13:55:22 -0800

    set static path for layout

--- app/controllers/spree/admin/orders_controller_decorator.rb
+++ app/controllers/spree/admin/orders_controller_decorator.rb
@@ -1,6 +1,7 @@
 Spree::Admin::OrdersController.class_eval do
   def print
+    load_order
     @hide_prices = params[:template] == "packaging_slip"
-    render :template => "admin/orders/#{params[:template] || "invoice"}.html.erb", :layout => 'pdfkit'
+    render :template => "spree/admin/orders/#{params[:template] || "invoice"}.html.erb", :layout => 'spree/layouts/pdfkit'
   end
 end
