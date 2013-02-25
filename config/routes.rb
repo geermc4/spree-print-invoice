51b2a798614c0a9942ec1d88e8694ad69e80c0ed
Ramón Roche <mrpollo@gmail.com>
2013-02-25 13:54:23 -0800

    fixed route

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
+#Spree::Core::Engine.routes.draw do
 Rails.application.routes.draw do
-  match "/admin/orders/:id/print", :to => 'admin/orders#print', :via => :get, :as => :admin_print_order
+  match "/admin/orders/:order_id/print", :to => 'spree/admin/orders#print', :via => :get, :as => :spree_admin_print_order
 end
