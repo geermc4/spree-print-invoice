804a26b3d7906dc6c89f064c30d542f7054f21a0
Ramón Roche <mrpollo@gmail.com>
2013-02-25 16:01:05 -0800

    Fixed render with locals

--- app/controllers/spree/admin/orders_controller_decorator.rb
+++ app/controllers/spree/admin/orders_controller_decorator.rb
@@ -2,6 +2,7 @@ Spree::Admin::OrdersController.class_eval do
   def print
     load_order
     @hide_prices = params[:template] == "packaging_slip"
-    render :template => "spree/admin/orders/#{params[:template] || "invoice"}.html.erb", :layout => 'spree/layouts/pdfkit'
+    #respond_with(@order)
+    render :layout => 'spree/layouts/pdfkit', :template => "spree/admin/orders/#{params[:template] || "invoice"}.html.erb", :locals => { :order => @order }
   end
 end
