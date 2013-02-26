34bf848a3e9475b374468aaa0cb4966bc8f3cb07
Ramón Roche <mrpollo@gmail.com>
2013-02-25 17:37:33 -0800

    Load order on before filter

--- app/controllers/spree/admin/orders_controller_decorator.rb
+++ app/controllers/spree/admin/orders_controller_decorator.rb
@@ -1,7 +1,7 @@
 Spree::Admin::OrdersController.class_eval do
+  append_before_filter :load_order, :only => [:print]
   def print
-    load_order
     @hide_prices = params[:template] == "packaging_slip"
-    render :layout => 'spree/layouts/pdfkit', :template => "spree/admin/orders/#{params[:template] || "invoice"}.html.erb"
+    render :pdf, :layout => 'spree/layouts/pdfkit', :template => "spree/admin/orders/#{params[:template] || "invoice"}.html.erb"
   end
 end
