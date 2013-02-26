f59eb7479e31c2b082dd8f47cba58e6209615016
Ramón Roche <mrpollo@gmail.com>
2013-02-25 16:18:42 -0800

    Load order now works

--- app/controllers/spree/admin/orders_controller_decorator.rb
+++ app/controllers/spree/admin/orders_controller_decorator.rb
@@ -2,7 +2,6 @@ Spree::Admin::OrdersController.class_eval do
   def print
     load_order
     @hide_prices = params[:template] == "packaging_slip"
-    #respond_with(@order)
-    render :layout => 'spree/layouts/pdfkit', :template => "spree/admin/orders/#{params[:template] || "invoice"}.html.erb", :locals => { :order => @order }
+    render :layout => 'spree/layouts/pdfkit', :template => "spree/admin/orders/#{params[:template] || "invoice"}.html.erb"
   end
 end
