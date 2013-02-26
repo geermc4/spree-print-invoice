85a7d646084e93eac102f6e3bfae6eaa9370cffa
Ramón Roche <mrpollo@gmail.com>
2013-02-26 14:08:46 -0800

    Route to allow print from user account page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 #Spree::Core::Engine.routes.draw do
 Rails.application.routes.draw do
   match "/admin/orders/:id/print", :to => 'spree/admin/orders#print', :via => :get, :as => :spree_admin_print_order
+  match "/orders/:id/print", :to => 'spree/admin/orders#print', :via => :get, :as => :spree_admin_print_order
 end
