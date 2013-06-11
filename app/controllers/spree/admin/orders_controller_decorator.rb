20f7dd07c163817a6c5c4712dff02492eb201285
geermc4 <geermc4@gmail.com>
2013-06-10 18:20:47 -0700

    rearanging stuff

--- app/controllers/spree/admin/orders_controller_decorator.rb
+++ app/controllers/spree/admin/orders_controller_decorator.rb
@@ -1,6 +1,6 @@
 Spree::Admin::OrdersController.class_eval do
-  append_before_filter :load_order, :only => [:print]
   def print
+    load_order
     @hide_prices = params[:template] == "packaging_slip"
     render :pdf, :layout => 'spree/layouts/pdfkit', :template => "spree/admin/orders/#{params[:template] || "invoice"}.html.erb"
   end
