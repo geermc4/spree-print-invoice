5d5749b3fdd7a87e4844fb11437340f3a0ef1a8c
Ramón Roche <mrpollo@gmail.com>
2013-08-15 22:28:26 -0700

    use shipment manifest to unify product display

--- app/controllers/spree/admin/invoices_controller.rb
+++ app/controllers/spree/admin/invoices_controller.rb
@@ -16,6 +16,10 @@ module Spree
 
       def load_objects
         @invoice = Spree::Invoice.new(params)
+        if !@invoice.valid?
+          # flash[:error] = @invoice.errors.messages
+          redirect_to admin_orders_path
+        end
         @order = @invoice.order
       end
     end
