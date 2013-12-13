c5e01fe2c9796a790f37dd55b4206663e9f8c4f9
Ruben Ascencio <galateaweb@gmail.com>
2013-12-12 16:20:31 -0800

    Use new append_routes method to avoid duplicated route error
    Also specify :only to avoid duplicating resource routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
-Spree::Core::Engine.routes.draw do
+Spree::Core::Engine.append_routes do
   scope module: :admin do
-    resources :orders do
+    resources :orders, :only => [] do
       get "/invoice" => "invoices#print"
     end
   end
   namespace :admin do
-    resources :orders do
+    resources :orders, :only => [] do
       get "/invoices" => "invoices#show"
       get "/invoices/complete" => "invoices#print"
       get "/invoices/shipments/:shipment_id" => "invoices#print", :as => "invoices_shipments"
